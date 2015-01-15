48315b7971b297ee03cf3aa31836860b7c923ff9
Igor Simdyanov <igorsimdyanov@gmail.com>
2015-01-15 14:33:48 +0300

    Миграция для создания пользователей

---
+++ db/migrate/20150115090911_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :password
+      t.timestamps
+    end
+  end
+end
