39fe3d6dd8576071a05c818a8f9670ede3809f14
Igor Simdyanov <igorsimdyanov@gmail.com>
2015-01-19 13:56:57 +0300

    Заполняем таблицы тестовыми данными

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,38 @@
 # Заполняем модель Lang
-def lang
+def langs
   # Очищаем таблицу langs
   Lang.destroy_all
   # Заполняем таблицу langs
-  Lang.create(title: "C++")
-  Lang.create([{title: "Ruby"},{title: "Python"}])
+  langs = [
+    {title: 'C++'},
+    {title: 'Ruby'},
+    {title: 'Python'}
+  ]
+  Lang.create(langs)
 end
 
-lang
+# Заполняем модель User
+def users
+  # Очищаем таблицу users
+  User.destroy_all
+  # Заполняем таблицу users
+  users = [
+    {name_first: 'Александр', name_second: 'Коробков', password: '321321'},
+    {name_first: 'Евгений', name_second: 'Понаморев', password: '321321'},
+    {name_first: 'Юрий', name_second: 'Кораблев', password: '321321'}
+  ]
+  User.create(users)
+end
+
+# Заполняем модель Snippet
+def snippets
+  Lang.all.each do |lng|
+    lng.snippets.build({title: "Первый сниппет по #{lng.title}", content: "Первый сниппет по #{lng.title}"})
+    lng.snippets.build({title: "Второй сниппет по #{lng.title}", content: "Второй сниппет по #{lng.title}"})
+    lng.save
+  end
+end
+
+langs
+users
+snippets
