e6b52fbdaadd2e9a65a158d4d199fabde12eee27
Igor Simdyanov <igorsimdyanov@gmail.com>
2015-01-19 14:32:34 +0300

    Заполнение модели данными

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,18 +17,19 @@ def users
   User.destroy_all
   # Заполняем таблицу users
   users = [
-    {name_first: 'Александр', name_second: 'Коробков', password: '321321'},
-    {name_first: 'Евгений', name_second: 'Понаморев', password: '321321'},
-    {name_first: 'Юрий', name_second: 'Кораблев', password: '321321'}
+    {first_name: 'Александр', last_name: 'Коробков', password: '321321'},
+    {first_name: 'Евгений', last_name: 'Понаморев', password: '321321'},
+    {first_name: 'Юрий', last_name: 'Кораблев', password: '321321'}
   ]
   User.create(users)
 end
 
 # Заполняем модель Snippet
 def snippets
+  usr = User.all
   Lang.all.each do |lng|
-    lng.snippets.build({title: "Первый сниппет по #{lng.title}", content: "Первый сниппет по #{lng.title}"})
-    lng.snippets.build({title: "Второй сниппет по #{lng.title}", content: "Второй сниппет по #{lng.title}"})
+    lng.snippets.build({title: "Первый сниппет по #{lng.title}", content: "Первый сниппет по #{lng.title}", user: usr.sample})
+    lng.snippets.build({title: "Второй сниппет по #{lng.title}", content: "Второй сниппет по #{lng.title}", user: usr.sample})
     lng.save
   end
 end
