5533eeed08ec7b492d4437783c4d3a7a6f8d4a18
Artem Nistratov <anistratov@go-promo.ru>
2015-01-28 14:37:51 +0300

    ADD: devise, :provider and :uid columns to User model, twitter omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :snippets
   has_one :image, as: :imageable
 end
