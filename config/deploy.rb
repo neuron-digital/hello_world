d200834e5f6d003cf6386d5017abf8f3cd80006c
Vadim Zharko <vzharko@gmail.com>
2015-02-06 13:39:03 +0300

    CHANGE: deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, 'hello_world'
 set :repo_url, 'git@github.com:Go-Promo/hello_world.git'
 set :git_shallow_clone, 1
 set :scm_verbose, true
+set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 set :ssh_options, {
   user: 'hello',
   forward_agent: true,
